0ad0c36a98dc54651bb2917d4e8b45053b8e0c27
Matheus Damião <matheus.damiaoliveira@gmail.com>
2022-07-04 14:00:10 -0300

    fixing image error

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -31,7 +31,7 @@ const BlogPostTemplate = ({ data, location }) => {
           itemProp="articleBody"
         />
         <hr />
-        <GatsbyImage image={post.frontmatter.imagem} alt="imagem"/>
+        
         <footer>
           <Bio />
         </footer>
