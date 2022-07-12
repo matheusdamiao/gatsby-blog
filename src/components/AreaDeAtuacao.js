3204ea5e4c4f396ac988ff3b85369724463e16cf
Matheus Damião <matheus.damiaoliveira@gmail.com>
2022-07-11 22:02:02 -0300

    trying to fix again

--- src/components/AreaDeAtuacao.js
+++ src/components/AreaDeAtuacao.js
@@ -8,23 +8,23 @@ import Botao from './Botao'
 const AreasDeAtuacao = () => {
 
 
-    const [cards, setCards] = useState([
-        {
-            icon: require('./../icons/icon-1.png'),
-            title: 'Guarda' ,
-            body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquam dolor ac nibh congue' ,
-            }, 
-            {
-            icon: require('./../icons/icon-2.png'),
-            title: 'Divórcio',
-            body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquam dolor ac nibh congue'   
-            },
-            {
-            icon: require('./../icons/icon-3.png'),
-            title: 'Pensão Alimentícia',
-            body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquam dolor ac nibh congue' 
-            },
-    ])
+    // const [cards, setCards] = useState([
+    //     {
+    //         icon: require('./../icons/icon-1.png'),
+    //         title: 'Guarda' ,
+    //         body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquam dolor ac nibh congue' ,
+    //         }, 
+    //         {
+    //         icon: require('./../icons/icon-2.png'),
+    //         title: 'Divórcio',
+    //         body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquam dolor ac nibh congue'   
+    //         },
+    //         {
+    //         icon: require('./../icons/icon-3.png'),
+    //         title: 'Pensão Alimentícia',
+    //         body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquam dolor ac nibh congue' 
+    //         },
+    // ])
 
 
 
@@ -59,9 +59,9 @@ const AreasDeAtuacao = () => {
     <div style={{display: 'flex', alignItems: 'center', flexWrap: 'nowrap', flexDirection: 'column'}} >
       <h2 style={{textAlign: 'center', fontWeight: 'light'}}>Áreas de Atuação</h2>
       <div style={cardsStyle}>
-        {cards.map( (card)=> {
+        {/* {cards.map( (card)=> {
             return <Card icon={card.icon} title={card.title}> {card.body}</Card>
-        })}
+        })} */}
       </div>
       <Botao size={'250px'} color={'F7EFAE'} height={'50px'} border={'none'} >
         Agende uma reunião
