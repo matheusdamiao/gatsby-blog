fdf1e931aea873b32f9054434815a72021b7b5bf
Matheus Damião <matheus.damiaoliveira@gmail.com>
2022-07-19 16:44:39 -0300

    adding slider

--- src/components/Depoimentos.js
+++ src/components/Depoimentos.js
@@ -1,7 +1,8 @@
-import { getImage } from 'gatsby-plugin-image'
-import React from 'react'
+import { getImage, StaticImage } from 'gatsby-plugin-image'
+import React, { useEffect, useState } from 'react'
 import Depoimento from './Depoimento'
 import { useStaticQuery, graphql } from 'gatsby'
+import Botao from './Botao'
 
 const Depoimentos = () => {
 
@@ -36,24 +37,41 @@ const Depoimentos = () => {
 
     const {avatar1, avatar2, avatar3} = data
 
-    
-   const depoimentos = [
-    {  
-     nome: 'Maria',
-     foto: getImage(avatar1),
-     texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquam dolor ac nibh congue, sed ultrices tortor fermentum.'
-    },
-    {  
-    nome: 'Jonas',
-    foto: getImage(avatar2),
-    texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquam dolor ac nibh congue, sed ultrices tortor fermentum.'
-    },
-     {  
-    nome: 'João',
-    foto: getImage(avatar3),
-    texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquam dolor ac nibh congue, sed ultrices tortor fermentum.'
-    }
-   ]
+    const [depoimentoState, setDepoimentoState] = useState([
+      {  
+       nome: 'Maria',
+       foto: getImage(avatar1),
+       texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquam dolor ac nibh congue, sed ultrices tortor fermentum.'
+      },
+      {  
+      nome: 'João',
+      foto: getImage(avatar2),
+      texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquam dolor ac nibh congue, sed ultrices tortor fermentum.'
+      },
+       {  
+      nome: 'Pedro',
+      foto: getImage(avatar3),
+      texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquam dolor ac nibh congue, sed ultrices tortor fermentum.'
+      },
+      {  
+        nome: 'Henrique',
+        foto: getImage(avatar2),
+        texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquam dolor ac nibh congue, sed ultrices tortor fermentum.'
+        },
+        {  
+          nome: 'Luana',
+          foto: getImage(avatar2),
+          texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquam dolor ac nibh congue, sed ultrices tortor fermentum.'
+          },
+          {  
+            nome: 'Zaara',
+            foto: getImage(avatar2),
+            texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquam dolor ac nibh congue, sed ultrices tortor fermentum.'
+            }    
+      
+     ])
+
+   
 
 
    const style = {
@@ -61,21 +79,48 @@ const Depoimentos = () => {
     flexWrap: 'wrap',
     alignItems: 'center',
     justifyContent: 'space-evenly',
-    maxWidth: '100%'
+    maxWidth: '100%',
+    // position: 'absolute'
    }
 
+   
 
+
+    const sliderDepoimento = (direcao)=> {
+    
+      if (direcao === 1) {
+        let removido = depoimentoState.shift();
+        depoimentoState.push(removido);
+        setDepoimentoState([...depoimentoState])
+        return
+      }
+      else { 
+
+        let ultimo = depoimentoState[depoimentoState.length - 1]
+        let removido = depoimentoState.pop()
+        setDepoimentoState([depoimentoState.splice(2, removido)])   
+        setDepoimentoState([depoimentoState.unshift(ultimo)])
+        setDepoimentoState([...depoimentoState])
+        console.log(removido)
+        return
+      }
+
+       }
+  
+   
 
 
   return (
-    <>
-    <h1 style={{textAlign: 'center', marginTop: '200px', marginBottom: '100px'}}> Depoimentos</h1>
-    <div style={style}>
-        {depoimentos.map( (card)=> {
-            return <Depoimento nome={card.nome} avatar={card.foto} text={card.texto}   />
-      })}
+    <div style={{position: 'absolute', width: '100%'}}>
+      <h1 style={{textAlign: 'center', marginTop: '200px', marginBottom: '100px'}}> Depoimentos</h1>
+      <div style={style}>
+          {depoimentoState.filter((value, index)=> index < 3).map( (card)=> {
+              return <Depoimento nome={card.nome} avatar={card.foto} text={card.texto}   />
+        })}
+      </div>
+      <button onClick={()=> sliderDepoimento(2)} style={{backgroundColor: 'white', position:'absolute', right: '95%', top: '70%', border: 'none', cursor: 'pointer' }}><StaticImage src="./../images/arrow-alt-circle-right-solid-2.png" /> </button>
+      <button onClick={()=> sliderDepoimento(1)} style={{backgroundColor: 'white', position:'absolute', left: '95%', top: '70%', border: 'none', cursor: 'pointer' }}><StaticImage src="./../images/arrow-alt-circle-right-solid.png" /> </button>
     </div>
-    </>
   )
 }
 
