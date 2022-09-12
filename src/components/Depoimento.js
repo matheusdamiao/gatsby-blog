f7e12f1be1e4543ba5abede5ef616565de5db7f5
Matheus Damião <matheus.damiaoliveira@gmail.com>
2022-09-12 18:29:28 -0300

    updating home

--- src/components/Depoimento.js
+++ src/components/Depoimento.js
@@ -3,52 +3,32 @@ import React from 'react'
 import Star from './../images/star.png'
 import quotesLeft from './../images/quotesLeft.png'
 import quotesRight from './../images/quotesRight.png'
-
+import * as style from './Depoimento.module.css'
 
 const Depoimento = ({nome, avatar, text, key}) => {
 
-    const style = {
-        display: 'flex',
-        flexDirection: 'column',
-        flexWrap: 'nowrap',
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
-        maxWidth: '700px',
-        width: '100%',
-        height: '100%',
-        maxHeight: '400px',
-        borderRadius: '46px',
-        textAlign: 'center',
-        margin: '10px',
-        padding: '10px',
-        border: 'none',
-        backgroundColor: 'rgba(244, 244, 244, 0.5)',
-        outline: 'none',
-        boxShadow: '-4px -4px 4px rgba(255, 255, 255, 0.5), 0px 4px 4px rgba(0, 0, 0, 0.15)',
-        
-    }
+  
 
   return (
-    <div style={style}>
-       <h4 style={{margin: '0', paddingRight: '20px'}}>{nome} </h4>
+    <div className={style.divDepoimento}>
+       <h4> {nome} </h4>
           <GatsbyImage
           image={avatar}
           alt='avatar'
           quality={100}
-          
           />
       
-      <div style={{display: 'flex'}}>
+      <div className={style.stars}>
         <img src={Star} alt=''/>
         <img src={Star} alt=''/>
         <img src={Star} alt=''/>
         <img src={Star} alt=''/>
         <img src={Star} alt=''/>
       </div>
-      <div>
-        {/* <img src={quotesLeft} style={{position: 'relative', right: '350px', top: '10px', margin: '0', padding: '0'}}   /> */}
-        <p style={{margin: '0 auto', width: '80%', textAlign: 'center' }}> {text} </p>
-        {/* <img src={quotesRight} style={{position: 'relative', left: '350px', margin: '0'}} /> */}
+      <div className={style.divQuote}>
+        <img className={style.quoteLeft} src={quotesLeft}/>
+        <p> {text} </p>
+        <img className={style.quoteRight} src={quotesRight} />
       </div>
       
         
--- src/components/Depoimentos.js
+++ src/components/Depoimentos.js
@@ -2,11 +2,10 @@ import { getImage, StaticImage } from 'gatsby-plugin-image'
 import React, { useEffect, useState } from 'react'
 import Depoimento from './Depoimento'
 import { useStaticQuery, graphql } from 'gatsby'
-import Botao from './Botao'
-import Icone from './../images/icone-depo.png'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper';
-
+import { Autoplay, EffectCards, EffectCreative, EffectFade, EffectFlip, Navigation, Pagination } from 'swiper';
+import * as style from './Depoimentos.module.css'
+import 'swiper/css/pagination'
 
 const Depoimentos = () => {
 
@@ -78,93 +77,68 @@ const Depoimentos = () => {
    
 
 
-   const style = {
-    display: 'flex',
-    flexWrap: 'wrap',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-    width: '100%',
-
-    // position: 'absolute'
-   }
-
-    // const sliderDepoimento = (direcao)=> {
-      
-
-    //   if (direcao === 1) {
-    //     let removido = depoimentoState.shift();
-    //     depoimentoState.push(removido);
-    //     setDepoimentoState([...depoimentoState])
-    //     return
-    //   }
-    //   else { 
-
-    //     let ultimo = depoimentoState[depoimentoState.length - 1]
-    //     let removido = depoimentoState.pop()
-    //     setDepoimentoState([depoimentoState.splice(2, removido)])   
-    //     setDepoimentoState([depoimentoState.unshift(ultimo)])
-    //     setDepoimentoState([...depoimentoState])
-    //     console.log(removido)
-    //     return
-    //   }
-
-    //    }
-  
-   
-
-
   return (
     <>
-    <div style={{position: 'relative', width: '100%', marginTop: '50px'}}>
-      <div style={{display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'center'}}>
-          <img src={Icone} />
-          <h1 style={{fontSize: '40px' ,textAlign: 'center', marginTop: '0px', marginBottom: '0px', fontFamily: 'Caudex'}}> Depoimentos</h1>
-      </div>
-      <div style={style}>
-        <Swiper
-        modules={[Navigation, Pagination]}
-        className='swiper-depoimento-principal'        
-        pagination={{clickable: 'true'}}
-        spaceBetween={50}
-       >
+    <div className={style.mainDiv}>
+      <div className={style.divTitulo}>
+          <h3> depoimentos </h3>
+          <h1> Os relatos de nossos clientes</h1>
 
-          <SwiperSlide className='swiper-depoimento'>
+      </div>
+      <div className={style.slider}>
+        <Swiper
+        modules={[Navigation, Pagination, EffectCards]}
+        className={style.swiperDepoimentoPrincipal}       
+        pagination={{
+          clickable:'true',
+          
+          
+      }}
+        gap={0}
+        Navigation
+        effect='cards'
+        cardsEffect={{slideShadows: false}}
+        
+        
+        >
+
+          <SwiperSlide className={style.swiperDepoimento}>
       
              <Depoimento nome={depoimentoState[0].nome} avatar={depoimentoState[0].foto} text={depoimentoState[0].texto} key={depoimentoState[0].index}  />
-             <Depoimento nome={depoimentoState[3].nome} avatar={depoimentoState[3].foto} text={depoimentoState[3].texto} key={depoimentoState[3].index}  />
       
           </SwiperSlide>
 
-          <SwiperSlide className='swiper-depoimento'>
-         
-             <Depoimento nome={depoimentoState[1].nome} avatar={depoimentoState[1].foto} text={depoimentoState[1].texto} key={depoimentoState[1].index}  />
-             <Depoimento nome={depoimentoState[4].nome} avatar={depoimentoState[4].foto} text={depoimentoState[4].texto} key={depoimentoState[4].index}  />
+          <SwiperSlide className={style.swiperDepoimento}>
+
+            <Depoimento nome={depoimentoState[1].nome} avatar={depoimentoState[3].foto} text={depoimentoState[3].texto} key={depoimentoState[3].index}  />
 
           </SwiperSlide>
 
-          <SwiperSlide className='swiper-depoimento'>
+          <SwiperSlide className={style.swiperDepoimento}>
          
-             <Depoimento nome={depoimentoState[2].nome} avatar={depoimentoState[2].foto} text={depoimentoState[2].texto} key={depoimentoState[2].index}  />
+             <Depoimento nome={depoimentoState[2].nome} avatar={depoimentoState[1].foto} text={depoimentoState[1].texto} key={depoimentoState[1].index}  />
+            
+          </SwiperSlide>
+
+
+          <SwiperSlide className={style.swiperDepoimento}>
+              <Depoimento nome={depoimentoState[3].nome} avatar={depoimentoState[4].foto} text={depoimentoState[4].texto} key={depoimentoState[4].index}  />
+          </SwiperSlide>
+
+          <SwiperSlide className={style.swiperDepoimento}>
+         
              <Depoimento nome={depoimentoState[5].nome} avatar={depoimentoState[5].foto} text={depoimentoState[5].texto} key={depoimentoState[5].index}  />
 
           </SwiperSlide>
 
         </Swiper>
 
+         
+      </div>
 
-          {/* {depoimentoState.filter((value, index)=> index < 3).map( (card)=> {
-              return <Depoimento nome={card.nome} avatar={card.foto} text={card.texto} key={card.index}  />
-        })} */}
-      </div>
-      <div style={{display: 'flex', width: '100%', justifyContent: 'space-between', position: 'absolute', top: '60%'}}>
-      {/* <button onClick={()=> sliderDepoimento(2)} style={{backgroundColor: 'white', border: 'none', cursor: 'pointer' }}><StaticImage src="./../images/arrow-alt-circle-right-solid-2.png" /> </button>
-      <button onClick={()=> sliderDepoimento(1)} style={{backgroundColor: 'white', border: 'none', cursor: 'pointer' }}><StaticImage src="./../images/arrow-alt-circle-right-solid.png" /> </button> */}
-      </div>
+      
     </div>
 
-    <div style={{display: 'flex', justifyContent: 'center', marginTop: '100px', marginBottom: '80px'}}>
-      <Botao size={'250px'} color={'rgb(247, 239, 174)'} height={'50px'} border={'none'}> Agende uma reunião</Botao>
-     </div>   
       </>
   )
 }
