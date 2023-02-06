a8a0ae945fa35d2535d345219d4b3f1e00428680
Matheus Damião <matheus.damiaoliveira@gmail.com>
2023-02-06 14:00:01 -0300

    style: add brand icon to gatsby plugin manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -135,7 +135,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
@@ -143,6 +143,6 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     `gatsby-plugin-netlify-cms`,
-    `gatsby-plugin-sass`
+    `gatsby-plugin-sass`,
   ],
 }
