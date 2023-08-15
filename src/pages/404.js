53fd1d7544fd73720f2811c1337c3426e3ea6c4c
matheus damiao <matheus.damiaoliveira@gmail.com>
2023-08-14 23:36:54 -0300

    adjust 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,16 @@
 import * as React from "react"
 import { graphql } from "gatsby"
-
-import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
 
   return (
-    <Layout location={location} title={siteTitle}>
+    <>
       <Seo title="404: Not Found" />
       <h1>Página não encontrada</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Layout>
+    </>
   )
 }
 
